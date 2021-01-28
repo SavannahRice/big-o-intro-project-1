b32f6086c171540a5e70ac0b2bbe3a8792616987
Savannah Rice <savannah.rice@gmail.com>
2021-01-27 17:34:31 -0700

    Finished 04

--- 04-execution-time-differences.js
+++ 04-execution-time-differences.js
@@ -17,30 +17,60 @@ function myMin2(list) {
   return min;
 }
 
+function addArr(array){
+  let sum = 0;
+  for (let i = 0; i < array.length; i++){
+    sum += array[i]
+  }
+  return sum;
+}
 
 function largestContiguousSubsum1(array) {
   const subArray = [];
 
-  for(let i = 0; i < array.length; i++){
-    for (let j = 0; j < array.length; j++){
+  for(let i = 0; i < array.length; i++){ //n
+    for (let j = i; j < array.length; j++){ //n
       if ( i === j ){
         subArray.push([array[i]]);
       } else {
         let subsubArray = [];
-        for ( let k = i; k <= j; k++){
+        for ( let k = i; k <= j; k++){ //
           subsubArray.push(array[k]);
         }
         subArray.push(subsubArray);
       }
     }
   }
+  const sumArray = [];
+  for (let l = 0; l < subArray.length; l++){
+    let sum = addArr(subArray[l]);
+    sumArray.push(sum);
+
+  }
+  return Math.max(...sumArray);
 }
 
 
 function largestContiguousSubsum2(array) {
-  // Code goes here ...
+  let largestSum = Math.max(...array);
+  let sum = 0;
+  for (let i = 0 ; i < array.length; i++){
+   
+
+    if (array[i] < 0) {
+      sum = 0;
+    } else {
+      sum += array[i]
+    }
+    if (sum > largestSum){
+      largestSum = sum;
+    }
+
+
+  }
+  return largestSum;
 }
 
 
-const list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-console.log(myMin2(list));  // =>  -5
+const list = [ 100, 3, 5, -10, 18, 14 ]; // [1, 3, -5, 10, 14] 18 
+console.log(largestContiguousSubsum2(list));  // =>  -5
